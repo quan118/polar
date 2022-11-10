8f2be9f24c2c3dd41366d8a9834f134d58d64916
khoi <git@khoi.io>
2022-11-10 11:07:33 +0700

    configure sentry for frontend

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
 import store from "./store";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://2d646750296a448f93f0613e48f8fee6@o4504132843864064.ingest.sentry.io/4504132846354432",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
